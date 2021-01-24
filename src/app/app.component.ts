c37d7b234097fb67e86eb5a114367bd7a34b1ae7
Thiago Martins de Freitas <martins86@gmail.com>
2021-01-24 17:35:07 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'poc-angular-tests-unit-e2e';
+}
