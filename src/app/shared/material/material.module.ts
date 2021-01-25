a3241be70056561f32b3996f3c74b6d1e00ecb5b
Thiago Martins de Freitas <46196327+martins86@users.noreply.github.com>
2021-01-25 15:43:50 -0300

    update material and add button module

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
 
+const materialModules = [
+  MatButtonModule
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    materialModules,
+  ],
+  exports: [
+    materialModules,
   ]
 })
 export class MaterialModule { }
